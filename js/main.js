4dbc731759cd869199b63b68deec21d0d04749bf
Sardor1009 <islomovsardor1009@gmail.com>
2023-12-07 10:23:57 +0500

    some changes

--- js/main.js
+++ js/main.js
@@ -1,10 +1,44 @@
 // problem1
 
-let a = 12;
-if (a > 0){
-   console.log(a += 1); 
+// let a = 12;
+// if (a > 0){
+//    console.log(a += 1); 
+// }
+// else {
+//     console.log(a);
+// }
+
+// p2 
+
+// let a = 0;
+
+// if (a > 0){
+//     a++;
+// }
+// else if (a < 0) {
+//     a -= 2;
+// }
+// else if (a == 0){
+//     a = 10;
+// }
+// console.log(a)
+
+// P3
+
+let a = -4;
+let b = -4; 
+let c = -3;
+
+positive = 0;
+
+if (a > 0 ) {
+    positive++;
 }
-else {
-    console.log(a);
+if ( b > 0){
+    positive++;
 }
+if (c > 0){
+    positive++;
+}
+console.log(positive)
 
